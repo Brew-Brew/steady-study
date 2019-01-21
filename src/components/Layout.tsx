8cf753f486e6cd0a592dc8b1fbc60de2ba7b0d75
seungkyu2 <zx6658@naver.com>
2019-01-21 23:15:41 +0900

    feat: :sparkles: edit default text

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,8 +12,8 @@ import { store } from "../store";
 
 export const menuItems = [
   { name: "Home", path: "/", exact: true, icon: "home", inverted: true },
-  { name: "About", path: "/about/", exact: true, icon: "info circle" },
-  { name: "Blog", path: "/blog/", exact: false, icon: "newspaper" },
+  { name: "who are you", path: "/about/", exact: true, icon: "info circle" },
+  { name: "My Thinking", path: "/blog/", exact: false, icon: "newspaper" }
 ];
 
 export interface LayoutProps {
@@ -30,26 +30,32 @@ const Layout = (props: LayoutProps) => {
   return (
     <Provider store={store}>
       <Sidebar.Pushable as={Segment}>
-
-        <SidebarMenu Link={Link} pathname={pathname} items={menuItems} visible={false} />
+        <SidebarMenu
+          Link={Link}
+          pathname={pathname}
+          items={menuItems}
+          visible={false}
+        />
 
         <Sidebar.Pusher style={{ minHeight: "100vh" }}>
           {/* Header */}
-          {isHome ? null : <HeaderMenu
-            Link={Link}
-            pathname={pathname}
-            items={menuItems}
-          />}
+          {isHome ? null : (
+            <HeaderMenu Link={Link} pathname={pathname} items={menuItems} />
+          )}
 
           {/* Render children pages */}
-          <div style={{ paddingBottom: 60 }}>
-            {props.children}
-          </div>
+          <div style={{ paddingBottom: 60 }}>{props.children}</div>
 
           {/* Footer */}
-          <Segment inverted vertical style={{ position: "absolute", bottom: 0, width: "100%" }}>
+          <Segment
+            inverted
+            vertical
+            style={{ position: "absolute", bottom: 0, width: "100%" }}
+          >
             <Container textAlign="center">
-              <p>Powered with <Icon name="heart" /> by Gatsby 2.0</p>
+              <p>
+                Powered with <Icon name="heart" /> by Gatsby 2.0
+              </p>
             </Container>
           </Segment>
         </Sidebar.Pusher>
@@ -60,7 +66,9 @@ const Layout = (props: LayoutProps) => {
 
 export default Layout;
 
-export const withLayout = <P extends object>(WrappedComponent: React.ComponentType<P>) =>
+export const withLayout = <P extends object>(
+  WrappedComponent: React.ComponentType<P>
+) =>
   class WithLayout extends React.Component<P & LayoutProps> {
     render() {
       return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,20 +8,32 @@ import {
   Container,
   Grid,
   Header,
-  Icon,
+  Icon
 } from "semantic-ui-react";
 
-const IndexPage = (props: LayoutProps) =>
+const IndexPage = (props: LayoutProps) => (
   <div>
     {/* Master head */}
     <Segment vertical inverted textAlign="center" className="masthead">
       <HeaderMenu
-        Link={Link} pathname={props.location.pathname} items={menuItems} inverted
+        Link={Link}
+        pathname={props.location.pathname}
+        items={menuItems}
+        inverted
       />
       <Container text>
-        <Header inverted as="h1">Gatsby 2.0 - Starter kit</Header>
-        <Header inverted as="h2">Typescript - Jest - Semantic UI</Header>
-        <Button primary size="huge">Get started!</Button>
+        <Header inverted as="h1">
+          Steady Study
+        </Header>
+        <Header inverted as="h2">
+          I am Ideveloper
+        </Header>
+        <Header inverted as="h3">
+          :) Idea + Developer
+        </Header>
+        <Button primary size="huge">
+          Welcome to my blog
+        </Button>
       </Container>
     </Segment>
 
@@ -30,29 +42,12 @@ const IndexPage = (props: LayoutProps) =>
       <Grid stackable verticalAlign="middle" className="container">
         <Grid.Row>
           <Grid.Column width="8">
-            <Header>Lorem ipsum</Header>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Porro laudantium ad, quae, perspiciatis ipsa distinctio.
-                </p>
-            <Header>Dolor sit amet</Header>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Porro laudantium ad, quae, perspiciatis ipsa distinctio.
-                </p>
+            <Header>Job Description</Header>
+            <p>I am Front-End Developer</p>
           </Grid.Column>
           <Grid.Column width="6" floated="right">
             {/* TODO replace with a pretty GIF */}
-            <Header>Lorem ipsum</Header>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Porro laudantium ad, quae, perspiciatis ipsa distinctio.
-                </p>
-            <Header>Dolor sit amet</Header>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Porro laudantium ad, quae, perspiciatis ipsa distinctio.
-                </p>
+            pretty animation what introduce me
           </Grid.Column>
         </Grid.Row>
       </Grid>
@@ -60,47 +55,52 @@ const IndexPage = (props: LayoutProps) =>
 
     {/* Key features */}
     <Segment vertical className="stripe alternate feature">
-      <Grid columns="3" textAlign="center" divided relaxed stackable className="container">
+      <Grid
+        columns="3"
+        textAlign="center"
+        divided
+        relaxed
+        stackable
+        className="container"
+      >
         <Grid.Row>
           <Grid.Column>
             <Header icon>
-              <Icon name="wizard"></Icon>
-              A kind of magic!
+              <Icon name="wizard" />A kind of magic!
             </Header>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptas eaque at quae cupiditate aspernatur quibusdam!
-                  Distinctio quod non, harum dolorum earum molestias,
-                  beatae expedita aliquam dolorem asperiores nemo amet quaerat.
-                </p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas
+              eaque at quae cupiditate aspernatur quibusdam! Distinctio quod
+              non, harum dolorum earum molestias, beatae expedita aliquam
+              dolorem asperiores nemo amet quaerat.
+            </p>
           </Grid.Column>
           <Grid.Column>
             <Header icon>
-              <Icon name="wizard"></Icon>
-              A kind of magic!
+              <Icon name="wizard" />A kind of magic!
             </Header>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptas eaque at quae cupiditate aspernatur quibusdam!
-                  Distinctio quod non, harum dolorum earum molestias,
-                  beatae expedita aliquam dolorem asperiores nemo amet quaerat.
-                </p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas
+              eaque at quae cupiditate aspernatur quibusdam! Distinctio quod
+              non, harum dolorum earum molestias, beatae expedita aliquam
+              dolorem asperiores nemo amet quaerat.
+            </p>
           </Grid.Column>
           <Grid.Column>
             <Header icon>
-              <Icon name="wizard"></Icon>
-              A kind of magic!
+              <Icon name="wizard" />A kind of magic!
             </Header>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptas eaque at quae cupiditate aspernatur quibusdam!
-                  Distinctio quod non, harum dolorum earum molestias,
-                  beatae expedita aliquam dolorem asperiores nemo amet quaerat.
-                </p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas
+              eaque at quae cupiditate aspernatur quibusdam! Distinctio quod
+              non, harum dolorum earum molestias, beatae expedita aliquam
+              dolorem asperiores nemo amet quaerat.
+            </p>
           </Grid.Column>
         </Grid.Row>
       </Grid>
     </Segment>
-  </div>;
+  </div>
+);
 
 export default withLayout(IndexPage);
