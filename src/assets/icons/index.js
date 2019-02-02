2658ccfce23a2cea3f6d4642576cd82c19d53293
seungkyu2 <zx6658@naver.com>
2019-02-02 19:50:09 +0900

    feat: :sparkles: about 페이지내용 추가

---
+++ src/assets/icons/index.js
@@ -0,0 +1,9 @@
+import github from './github.png';
+import mail from './mail.png';
+import tistory from './tistory.png';
+
+export {
+    github,
+    mail,
+    tistory
+};
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,44 @@
 import * as React from "react";
 import { Header, Container, Segment, Icon } from "semantic-ui-react";
 import { withLayout } from "../components/Layout";
+import { github, mail, tistory } from "../assets/icons";
+import styled, { keyframes } from "styled-components";
+
+const InfoWrapper = styled.div`
+  margin: 10px;
+`;
+
+const ImgIcon = styled.img`
+  width: 24px;
+  height: 24px;
+  margin-right: 10px;
+  vertical-align: middle;
+`;
+
+const IconLabel = styled.p`
+  font-weight: 600;
+  font-size: 16px;
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+const abooutInfo = [
+  {
+    img: github,
+    label: "Github",
+    link: "",
+  },
+  {
+    img: mail,
+    label: "E-mail: zx6658@gmail.com",
+    link: "",
+  },
+  {
+    img: tistory,
+    label: "Tistory blog",
+    link: "",
+  },
+];
 
 const AboutPage = () => {
   return (
@@ -11,9 +49,15 @@ const AboutPage = () => {
         </Header>
       </Segment>
       <Segment vertical>
-        <p>Github</p>
-        <p>Tistory</p>
-        <p>Email</p>
+        {abooutInfo.map((info) => {
+          return (
+            <InfoWrapper>
+              <ImgIcon src={info.img} />
+              <IconLabel>{info.label}</IconLabel>
+            </InfoWrapper>
+          );
+        })}
+        <div />
       </Segment>
     </Container>
   );
