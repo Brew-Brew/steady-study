63cbd0a8cbdb888b2e23e4db6d40648f56c638f6
seungkyu2 <zx6658@naver.com>
2019-03-08 00:11:45 +0900

    feat: :hammer: delete useless section

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,21 +59,6 @@ module.exports = {
     // Add typescript stack into webpack
     `gatsby-plugin-typescript`,
 
-    // This plugin takes your configuration and generates a
-    // web manifest file so your website can be added to your
-    // homescreen on Android.
-    /* eslint-disable camelcase */
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby website`,
-        short_name: `Gatsby website`,
-        start_url: `/`,
-        background_color: `#f7f7f7`,
-        theme_color: `#191919`,
-        display: `minimal-ui`
-      }
-    },
     /* eslint-enable camelcase */
 
     // This plugin generates a service worker and AppShell
