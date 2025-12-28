4646c14fff196e1331b98f36d712cd2a598836b5
Brew-Brew <zx6658@naver.com>
2025-12-29 02:02:09 +0900

    refactor: remove Redux Provider from gatsby-browser.tsx and delete gatsby-ssr.tsx; update dependencies in package.json

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,10 +1,3 @@
-import * as React from "react";
-import { Provider } from "react-redux";
-
-
-import { store } from "./src/store";
-
-export const wrapRootElement = ({ element }) =>
-    <Provider store={store} >
-      {element}
-    </Provider>;
+// gatsby-browser.tsx
+// gatsby-plugin-styled-components가 자동으로 StyleSheetManager를 처리합니다
+// Layout.tsx에서 이미 Provider를 사용하고 있으므로 여기서는 필요 없음
